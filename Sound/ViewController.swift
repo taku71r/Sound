b386dab39a943ac616882a64950f1095dc667555
Takuroh Shimizu <his7716twd@gmail.com>
2020-05-13 12:41:06 +0900

    サウンドアプリの完成

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import AVFoundation
+
+
+
+
 
 class ViewController: UIViewController {
+    
+    //ドラムのサウンドファイルを読み込んでプレイヤーを作る
+    let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    //ドラムがタップされた時に呼ばれる
+    @IBAction func tapDrumbutton() {
+        
+        //ドラムの音を巻き戻す
+        drumSoundPlayer.currentTime = 0
+        //ドラムの音を再生する
+        drumSoundPlayer.play()
+        
+        
+        
     }
 
 
